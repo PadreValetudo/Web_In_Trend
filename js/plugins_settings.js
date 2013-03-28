f3c94def195174c14427427843707f97a95fb6bf
Bogdan92 <bogdan.ostapchuk@gmail.com>
2013-03-28 20:44:22 +0200

    First commit

---
+++ js/plugins_settings.js
@@ -0,0 +1,12 @@
+/***** main slider ******************************************************/
+$(function() {
+
+    $('#da-slider').cslider({
+        autoplay	: true,     //automatic slide change
+        bgincrement	: 450,      //background shift
+        interval    : 7000      //slide change time
+    });
+
+});
+/***** end of main slider ******************************************************/
+
